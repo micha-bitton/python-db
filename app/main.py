897e687cd2c7e6858b69dac53936cf6e80232a2b
micha.bitton@biocatch.com <micha.bitton@biocatch.com>
2025-03-26 10:50:47 +0200

    adding coding interview

---
+++ app/main.py
@@ -0,0 +1,28 @@
+import time
+import psycopg2
+
+def main():
+    print("⏳ Waiting before connecting...")
+    time.sleep(2)
+
+    try:
+        conn = psycopg2.connect(
+            dbname="demo",
+            user="user",
+            password="password",
+            host="localhost",  # <--- connect to container via published port
+            port=5432
+        )
+        print("✅ Connected to DB!")
+
+        cur = conn.cursor()
+        conn.commit()
+        cur.close()
+        conn.close()
+        print("✅ Inserted data.")
+
+    except Exception as e:
+        print(f"❌ Connection failed: {e}")
+
+if __name__ == "__main__":
+    main()
